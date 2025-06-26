90ddc75d831c1c3a575119e3abb472860c260f93
PreferenceApp <preferenceapp@gmail.com>
2025-06-26 11:37:23 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,85 +3,60 @@ import { Client, Users } from 'node-appwrite';
 
 export default async (context) => {
   const { req, res, log, error } = context;
-
-  
-  /* Start of create stripe checkout session*/
   const stripeClient = stripe(process.env.STRIPE_SECRET_KEY);
 
-  const createCheckoutSession = async (userId, successUrl, failureUrl) => {
-
-     const lineItems = [
-      {
-        price_data: {
-          unit_amount: 500, // $5.00
-          currency: 'usd',
-          product_data: {
-            name: '1 Week',
-          },
+   const lineItems = [
+    {
+      price_data: {
+        unit_amount: 500, // $5.00
+        currency: 'usd',
+        product_data: {
+          name: '1 Week',
         },
-        quantity: 1,
       },
-      {
-        price_data: {
-          unit_amount: 1000, // $10.00
-          currency: 'usd',
-          product_data: {
-            name: '1 Month',
-          },
+      quantity: 1,
+    },
+    {
+      price_data: {
+        unit_amount: 1000, // $10.00
+        currency: 'usd',
+        product_data: {
+          name: '1 Month',
         },
-        quantity: 1,
       },
-      {
-        price_data: {
-          unit_amount: 2500, // $25.00
-          currency: 'usd',
-          product_data: {
-            name: '3 Months',
-          },
+      quantity: 1,
+    },
+    {
+      price_data: {
+        unit_amount: 2500, // $25.00
+        currency: 'usd',
+        product_data: {
+          name: '3 Months',
         },
-        quantity: 1,
       },
-      {
-        price_data: {
-          unit_amount: 4500, // $45.00
-          currency: 'usd',
-          product_data: {
-            name: '6 Months',
-          },
+      quantity: 1,
+    },
+    {
+      price_data: {
+        unit_amount: 4500, // $45.00
+        currency: 'usd',
+        product_data: {
+          name: '6 Months',
         },
-        quantity: 1,
       },
-      {
-        price_data: {
-          unit_amount: 8500, // $85.00
-          currency: 'usd',
-          product_data: {
-            name: '1 Year',
-          },
+      quantity: 1,
+    },
+    {
+      price_data: {
+        unit_amount: 8500, // $85.00
+        currency: 'usd',
+        product_data: {
+          name: '1 Year',
         },
-        quantity: 1,
-      }
-    ];
-    
-    try {
-      const body = JSON.parse(req.body);
-      return await stripeClient.checkout.sessions.create({
-        payment_method_types: ['card'],
-        line_items: [lineItems[body.item]],
-        success_url: successUrl,
-        cancel_url: failureUrl,
-        client_reference_id: userId,
-        metadata: { 
-          userId: userId,
-          item: body.item.toString()
-        },
-        mode: 'payment',
-      });
-    } catch (err) {
-      error('Stripe checkout error:', err);
-      return null;
+      },
+      quantity: 1,
     }
-  };
+  ];
 
   if (req.method === 'POST' && req.path === '/checkout') {
     const baseUrl = process.env.ENDPOINT_URL;
@@ -94,14 +69,32 @@ export default async (context) => {
       return res.json({ error: 'Missing user ID' }, 400);
     }
     
-    const session = await createCheckoutSession(userId, successUrl, failureUrl);
-    if (!session || !session.url) {
-      error('Failed to create Stripe session.');
-      return res.json({ error: 'Stripe session creation failed' }, 500);
-    }
+     try {
+      const body = JSON.parse(req.body);
+      const session = await stripeClient.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: [lineItems[body.item]],
+        success_url: successUrl,
+        cancel_url: failureUrl,
+        client_reference_id: userId,
+        metadata: { 
+          userId: userId,
+          item: body.item.toString()
+        },
+        mode: 'payment',
+      });
 
-    log(`Created Stripe checkout session for user ${userId}`);
-    return res.json({ url: session.url }, 200);
+      if (!session || !session.url) {
+        error('Failed to create Stripe session.');
+        return res.json({ error: 'Stripe session creation failed' }, 500);
+      }
+  
+      log(`Created Stripe checkout session for user ${userId}`);
+      return res.json({ url: session.url }, 200);
+    } catch (err) {
+      error('Stripe checkout error:', err);
+      return null;
+    }
   }
   
   // === Fallback ===
